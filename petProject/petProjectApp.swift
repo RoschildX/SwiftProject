e261e37ceda4068936ad2546ec487d345338a54c
farhan <mohamad.farhan@mcruncher.com>
2024-06-28 15:10:01 +0800

    Initial Commit

---
+++ petProject/petProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  petProjectApp.swift
+//  petProject
+//
+//  Created by Mohamad Farhan Bin Roslan on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct petProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ petProjectUITests/petProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  petProjectUITestsLaunchTests.swift
+//  petProjectUITests
+//
+//  Created by Mohamad Farhan Bin Roslan on 28/06/2024.
+//
+
+import XCTest
+
+final class petProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
